829c5bbd01e5967fa9c51fc45f847064e84443ba
chicacode <gerahernan.al@gmail.com>
2022-02-07 07:25:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div>
+        <h1 className="text-3xl font-bold text-center">
+          What's the plan for Today
+        </h1>
+      </div>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+      </TodoList>
+      <CreateTodoButton />
     </div>
+
   );
 }
 
